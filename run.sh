f5432ed1a1925ad9119f807d80b060f6e34af648
7d <61975820+iiiii7d@users.noreply.github.com>
2024-07-09 19:45:31 +0800

    add fwc and sdz

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ set -euxo pipefail
 
 ./flight-network-planner run config.yml --old out.txt -s > out-new.txt
 mv out-new.txt out.txt
-./flight-network-planner gate-keys out.txt > gates.txt
+./flight-network-planner gate-keys out.txt > gates2.txt
