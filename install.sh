a60417c82cd956cb3c5ddf5d609271d550c2e4fe
7d <61975820+iiiii7d@users.noreply.github.com>
2024-07-27 13:23:59 +0800

    add lnt

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-set -euxo pipefail
+!set -euxo pipefail
 
 curl "https://github.com/MRT-Aurora-Air/flight-network-planner/releases/download/v1.1.1/flight-network-planner-ubuntu" -Lo flight-network-planner
 chmod +x flight-network-planner
