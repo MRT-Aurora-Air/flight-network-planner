4d33b5d34f11d93698d63059d9498e6cc3e38377
7d <61975820+iiiii7d@users.noreply.github.com>
2024-12-13 00:23:49 +0800

    updates for 1.2.1

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 set -euxo pipefail
 
-./flight-network-planner run config.yml --old out.txt -s > out-new.txt
-mv out-new.txt out.txt
+./flight-network-planner run config.yml -sro out.txt
 ./flight-network-planner gate-keys out.txt > gates2.txt
