ef7cde3cdefb6e35806903d0d043954f3ea60305
7d <61975820+iiiii7d@users.noreply.github.com>
2025-01-05 23:37:11 +0800

    use gatelogue-types

--- src/types/flight_data.rs
+++ src/types/flight_data.rs
@@ -1,13 +1,11 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use anyhow::{anyhow, Result};
+use gatelogue_types::{GatelogueData, World};
 use itertools::Itertools;
 use log::{debug, info, warn};
 
 use crate::types::{config::Config, *};
-
-const GATELOGUE_URL: &str =
-    "https://raw.githubusercontent.com/MRT-Map/gatelogue/refs/heads/dist/data_no_sources.json";
 
 #[allow(dead_code)]
 #[derive(Debug)]
@@ -28,133 +26,50 @@ pub struct FlightData {
 impl FlightData {
     pub fn from_gatelogue() -> Result<Self> {
         info!("Downloading gatelogue data");
-        let bytes = reqwest::blocking::get(GATELOGUE_URL)?.bytes()?;
-        let data = serde_json::from_slice::<serde_json::Value>(&bytes)?;
-        let data = data
-            .get("nodes")
-            .ok_or(anyhow!("No `nodes`"))?
-            .as_object()
-            .ok_or(anyhow!("`nodes` not object"))?;
-
-        macro_rules! filter_from_data {
-            ($data:ident, $ty:literal) => {
-                $data
-                    .values()
-                    .map(|a| {
-                        Ok((
-                            a,
-                            a.get("type")
-                                .ok_or(anyhow!("No `type`"))?
-                                .as_str()
-                                .ok_or(anyhow!("`type` not string"))?,
-                        ))
-                    })
-                    .filter_ok(|(_, ty)| *ty == $ty)
-                    .map_ok(|(a, _)| a)
-                    .collect::<Result<Vec<_>>>()?
-                    .into_iter()
-            };
-        }
+        let data = GatelogueData::ureq_get_no_sources()?;
 
         info!("Processing gatelogue data");
-        let flights = filter_from_data!(data, "AirFlight")
+        let flights = data
+            .nodes
+            .values()
+            .filter_map(|a| a.as_air_flight())
             .map(|a| {
-                let airline_id = a
-                    .get("airline")
-                    .ok_or(anyhow!("No `airline`"))?
-                    .as_number()
-                    .ok_or(anyhow!("`airline` not number"))?;
-                let airline_name = data
-                    .get(&airline_id.to_string())
-                    .ok_or(anyhow!("airline {airline_id} does not exist"))?
-                    .get("name")
-                    .ok_or(anyhow!("No `name`"))?
-                    .as_str()
-                    .ok_or(anyhow!("`name` not string"))?;
+                let airline_name = data.get_air_airline(*a.airline)?.name.to_owned().into();
 
-                let flight_number = a
-                    .get("codes")
-                    .ok_or(anyhow!("No `codes`"))?
-                    .get(0)
-                    .ok_or(anyhow!("No codes"))?
-                    .as_str()
-                    .ok_or(anyhow!("`codes` elements not string"))?;
+                let flight_number = a.codes.first().ok_or(anyhow!("No codes"))?.into();
 
                 let airport_codes = a
-                    .get("gates")
-                    .ok_or(anyhow!("No `gates`"))?
-                    .as_array()
-                    .ok_or(anyhow!("`gates` not array"))?
+                    .gates
                     .iter()
                     .map(|a| {
-                        let gate_id = a
-                            .as_number()
-                            .ok_or(anyhow!("`gates` elements not number"))?;
-                        let airport_id = data
-                            .get(&gate_id.to_string())
-                            .ok_or(anyhow!("gate {gate_id} does not exist"))?
-                            .get("airport")
-                            .ok_or(anyhow!("No `airport`"))?
-                            .as_number()
-                            .ok_or(anyhow!("`airport` not number"))?;
-                        let airport_code = data
-                            .get(&airport_id.to_string())
-                            .ok_or(anyhow!("airport {airport_id} does not exist"))?
-                            .get("code")
-                            .ok_or(anyhow!("No `code`"))?
-                            .as_str()
-                            .ok_or(anyhow!("`code` not string"))?;
-                        Ok(airport_code.into())
+                        let airport_id = *data.get_air_gate(**a)?.airport;
+                        Ok(data.get_air_airport(airport_id)?.code.to_owned().into())
                     })
                     .collect::<Result<Vec<_>>>()?;
 
                 Ok(FlightDataFlight {
-                    airline: airline_name.into(),
-                    flight_number: flight_number.into(),
+                    airline: airline_name,
+                    flight_number,
                     airports: airport_codes,
                 })
             })
             .collect::<Result<Vec<_>>>()?;
 
-        let old_world_airports = filter_from_data!(data, "AirAirport")
-            .map(|a| {
-                Ok((
-                    a,
-                    a.get("world")
-                        .ok_or(anyhow!("No `world`"))?
-                        .as_str()
-                        .unwrap_or("New"),
-                ))
-            })
-            .filter_ok(|(_, world)| *world == "Old")
-            .map_ok(|(a, _)| {
-                Ok(a.get("code")
-                    .ok_or(anyhow!("No `code`"))?
-                    .as_str()
-                    .ok_or(anyhow!("`code` not string"))?
-                    .into())
-            })
-            .collect::<Result<Result<Vec<_>>>>()??;
+        let old_world_airports = data
+            .nodes
+            .values()
+            .filter_map(|a| a.as_air_airport())
+            .filter(|a| a.common.world.as_ref().is_some_and(|a| **a == World::Old))
+            .map(|a| a.code.to_owned().into())
+            .collect();
 
-        let new_world_airports = filter_from_data!(data, "AirAirport")
-            .map(|a| {
-                Ok((
-                    a,
-                    a.get("world")
-                        .ok_or(anyhow!("No `world`"))?
-                        .as_str()
-                        .unwrap_or("New"),
-                ))
-            })
-            .filter_ok(|(_, world)| *world == "New")
-            .map_ok(|(a, _)| {
-                Ok(a.get("code")
-                    .ok_or(anyhow!("No `code`"))?
-                    .as_str()
-                    .ok_or(anyhow!("`code` not string"))?
-                    .into())
-            })
-            .collect::<Result<Result<Vec<_>>>>()??;
+        let new_world_airports = data
+            .nodes
+            .values()
+            .filter_map(|a| a.as_air_airport())
+            .filter(|a| a.common.world.as_ref().is_none_or(|a| **a == World::New))
+            .map(|a| a.code.to_owned().into())
+            .collect();
 
         Ok(Self {
             flights,
